9bd0683ee3269cd49d6068ae023c679f9ffd201b
Pan Alexey <pan-alexey@ozon.ru>
2021-02-18 09:40:19 +0300

    Add structure

---
+++ packages/core/src/components/Terminal.ts
@@ -0,0 +1,38 @@
+import ips from '../helpers/networks';
+
+/*
+------------------------------------
+Application
+client: [xxxxxxxxxx]
+server: [xxxxxxxxxx]
+
+Debug:
+client: [xxxxxxxxxx]
+server: [xxxxxxxxxx]
+
+Debug was start:
+http://127.0.0.1:8080;
+http://192.168.100.255:8080;
+------------------------------------
+Try start server:
+*/
+
+class Terminal {
+  //
+
+  constructor(names:Array<string>) {
+  }
+
+  public start(name:string) {}
+  public error(name:string) {}
+  public success(name: string, hash: string) {}
+
+  public server(port: string) {
+    // ips
+  }
+
+  private clear() {
+    process.stdout.write(process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H');
+  }
+  private output() {}
+}
---
+++ packages/core/src/helpers/networks.ts
@@ -0,0 +1,16 @@
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+const ips = [];
+
+Object.keys(ifaces).forEach(function (ifname) {
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+    ips.push(iface.address);
+  });
+});
+
+export default ips;
