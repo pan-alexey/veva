4451a764248a93a832bd229162342768ed92364f
Aleksey Pan <agpan@avito.ru>
2022-07-27 17:18:29 +0300

    update versions

--- examples/webpack/src/components/Text.tsx
+++ examples/webpack/src/components/Text.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./style.module.scss";
 
-const a: null = 1;
-const b: null = 1;
+// const a: null = 1;
+// const b: null = 1;
 const Text: React.FC = () => {
   return <h1 className={styles.text}>with HMR!!!</h1>
 };
